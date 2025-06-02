7e3213398833b29c54859eafa198dbf383ed7e60
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2025-06-02 17:30:21 +0200

    feat: replace url on start.sh file

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Start Chromium in fullscreen mode and navigate to website in fullscreen mode
-chromium-browser --force-device-scale-factor=1 --start-fullscreen "https://alexpagnotta.dev" 
+chromium-browser --force-device-scale-factor=1 --start-fullscreen "{URL_HERE}" 
